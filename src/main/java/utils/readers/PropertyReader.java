6b0411a604d26114204887c1818563d460a0bb83
Arsentii_Chander <arsentiy_ch@mail.ru>
2021-10-23 09:25:35 +0300

    add firefox support

--- src/main/java/utils/readers/PropertyReader.java
+++ src/main/java/utils/readers/PropertyReader.java
@@ -13,34 +13,43 @@ import java.util.Properties;
 
 public class PropertyReader {
 
-  private static final Properties PROPERTIES = new Properties();
+    private static final Properties PROPERTIES = new Properties();
 
-  public static void readProperties() {
-    PROPERTIES.putAll(System.getProperties());
-    PROPERTIES.putAll(System.getenv());
-
-    List<Class<?>> classesList = new ClassLoader().loadClassesInPackage("utils.properties");
-    String filePath;
-    for (Class<?> clazz : classesList) {
-      Field[] fields = FieldUtils.getFieldsWithAnnotation(clazz, Property.class);
-      if (clazz.isAnnotationPresent(FilePath.class)) {
-        filePath = clazz.getAnnotation(FilePath.class).value();
-        try {
-          PROPERTIES.load(new FileInputStream(filePath));
-        } catch (IOException e) {
-          iLogger.error("Couldn't read file " + filePath, e);
-        }
-      }
-      for (Field field : fields) {
-        String key = field.getAnnotation(Property.class).value();
-        try {
-          String value = PROPERTIES.getProperty(key);
-          field.set(String.class, value);
-          iLogger.info("Set property " + clazz.getName() + "::" + field.getName() + " with value " + value);
-        } catch (IllegalAccessException e) {
-          iLogger.error("Couldn't set field " + field.getName() + " for property class " + clazz.getName(), e);
-        }
-      }
+    public static void readProperties() {
+        List<Class<?>> classesList = new ClassLoader().loadClassesInPackage("utils.properties");
+        loadPropertiesFromAllSources(classesList);
+        populatePropertyFieldValues(classesList);
     }
-  }
+
+    private static void populatePropertyFieldValues(List<Class<?>> classesList) {
+        for (Class<?> clazz : classesList) {
+            Field[] fields = FieldUtils.getFieldsWithAnnotation(clazz, Property.class);
+            for (Field field : fields) {
+                String key = field.getAnnotation(Property.class).value();
+                try {
+                    String value = PROPERTIES.getProperty(key);
+                    field.set(String.class, value);
+                    iLogger.info("Set property " + clazz.getName() + "::" + field.getName() + " with value " + value);
+                } catch (IllegalAccessException e) {
+                    iLogger.error("Couldn't set field " + field.getName() + " for property class " + clazz.getName(), e);
+                }
+            }
+        }
+    }
+
+    private static void loadPropertiesFromAllSources(List<Class<?>> classesList) {
+        String filePath;
+        for (Class<?> clazz : classesList) {
+            if (clazz.isAnnotationPresent(FilePath.class)) {
+                filePath = clazz.getAnnotation(FilePath.class).value();
+                try {
+                    PROPERTIES.load(new FileInputStream(filePath));
+                } catch (IOException e) {
+                    iLogger.error("Couldn't read file " + filePath, e);
+                }
+            }
+        }
+        PROPERTIES.putAll(System.getProperties());
+        PROPERTIES.putAll(System.getenv());
+    }
 }
