bcdc4cc9c607876967277225f0e0a2c44f85504d
Akio Nkodia <AKIONK@ext.inditex.com>
2021-10-23 11:13:00 +0300

    Implement getHistoricPrices

--- src/test/java/com/hackathon/testproject/pages/nyse/QuotePage.java
+++ src/test/java/com/hackathon/testproject/pages/nyse/QuotePage.java
@@ -3,6 +3,7 @@ package com.hackathon.testproject.pages.nyse;
 import com.hackathon.testproject.model.HistoricPrice;
 import com.hackathon.testproject.model.Period;
 import com.hackathon.testproject.pages.AbstractPage;
+import core.web.iElementsList;
 import core.web.iWebElement;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.support.FindBy;
@@ -24,6 +25,12 @@ public class QuotePage extends AbstractPage<QuotePage> {
     @FindBy(xpath = "//button[span[contains(text(),'GO')]]")
     private iWebElement goBtn;
 
+    @FindBy(css = ".DataTable-nyse .Time .data-table-cell")
+    private iElementsList historicPricesDateValues;
+
+    @FindBy(css = ".DataTable-nyse .Close [class*='table-cell-price']")
+    private iElementsList historicPricesCloseValues;
+
     public void setHistoricPricesRange(Period period) {
         fromDateInput.setFocus();
         setDate(fromDateInput, period.getStartDate());
@@ -31,16 +38,22 @@ public class QuotePage extends AbstractPage<QuotePage> {
         goBtn.click();
     }
 
+    public List<HistoricPrice> getHistoricPrices() {
+        List<HistoricPrice> historicPrices = new ArrayList<>();
+        for (int rowInd = 0; rowInd < historicPricesDateValues.size(); ++rowInd) {
+            historicPrices.add(HistoricPrice.builder()
+                    .date(historicPricesDateValues.get(rowInd).getText())
+                    .value(historicPricesCloseValues.get(rowInd).getText())
+                    .build());
+        }
+        return historicPrices;
+    }
+
     private void setDate(iWebElement dateInput, String date) {
         dateInput.sendKeys(Keys.CONTROL, "a");
         dateInput.sendKeys(Keys.DELETE);
         dateInput.setText(date);
         selectedDayBtn.click();
-    }
-
-    public List<HistoricPrice> getHistoricPrices() {
-        List<HistoricPrice> historicPrices = new ArrayList<>();
-        return historicPrices;
     }
 }
 
