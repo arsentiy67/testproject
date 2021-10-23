fdb220b75238c55ee228b5e7bf18dd9badc5bfda
Arsentii_Chander <arsentiy_ch@mail.ru>
2021-10-23 10:06:33 +0300

    add firefox support

--- src/main/java/utils/logging/TestListener.java
+++ src/main/java/utils/logging/TestListener.java
@@ -20,7 +20,7 @@ public class TestListener extends TestListenerAdapter {
 
     @Override
     public void onTestSuccess(ITestResult tr) {
-        iLogger.info("Test success " + tr.getTestName());
+        iLogger.info("Test success " + tr.getMethod());
         iLogger.takeScreenshot();
     }
 }
