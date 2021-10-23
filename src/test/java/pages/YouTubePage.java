228eace7dfb3c0bdab5a98e440079ec17ca8f8cb
Kristina_Chander <Kristina.Chander@syngenta.com>
2021-10-23 10:49:46 +0300

    7  + beginning of 8

--- src/test/java/pages/YouTubePage.java
+++ src/test/java/pages/YouTubePage.java
@@ -1,7 +1,13 @@
 package pages;
 
+import core.DriverFactory;
 import core.web.iWebElement;
+import lombok.SneakyThrows;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import utils.logging.iLogger;
 
 import java.util.List;
@@ -11,7 +17,8 @@ public class YouTubePage extends AbstractPage {
     private iWebElement searchBar;
     @FindBy(xpath = "//ytd-channel-renderer//yt-formatted-string[@id='text' and text()='%s']")
     private iWebElement searchChannelName;
-    @FindBy(css = "a#video-title")
+    //@FindBy(css = "a#video-title")
+    @FindBy(xpath = "//yt-formatted-string[@aria-label]")
     private List<iWebElement> videoTitles;
 
     public YouTubePage(String pageUrl) {
@@ -34,8 +41,17 @@ public class YouTubePage extends AbstractPage {
         searchChannelName.template(channelName).click();
     }
 
+    @SneakyThrows
     public void clickMatchingVideo(String title) {
-        iWebElement titleOnPage = videoTitles.stream().filter(s -> s.getAttribute("title").contains(title)).findFirst().get();
+//        new WebDriverWait(DriverFactory.getCurrentDriver(), 30)
+//                .until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//yt-formatted-string[@aria-label]")));
+        Thread.sleep(3000);
+        System.out.println(videoTitles.size());
+        iWebElement titleOnPage = videoTitles
+                .stream()
+                .filter(s -> s.getAttribute("aria-label").contains(title))
+                .findFirst()
+                .orElseThrow();
         titleOnPage.click();
     }
 }
